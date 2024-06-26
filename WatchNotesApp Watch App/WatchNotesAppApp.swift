967170fe0206cc13386aef08494036e9e7cfd8b7
lucasleone03 <lucasagustinleone2003@gmail.com>
2024-06-26 20:36:39 -0300

    Initial Commit

---
+++ WatchNotesApp Watch App/WatchNotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchNotesAppApp.swift
+//  WatchNotesApp Watch App
+//
+//  Created by Lucas Leone on 26/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct WatchNotesApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WatchNotesApp/WatchNotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchNotesAppApp.swift
+//  WatchNotesApp
+//
+//  Created by Lucas Leone on 26/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct WatchNotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
